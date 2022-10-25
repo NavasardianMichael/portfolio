6e469844baa940744547a15083b797dcc7e13f3c
NavasardianMichael <navasardianmichael@gmail.com>
2022-10-26 00:29:52 +0400

    publicationspartially filled

--- src/components/Publications/Publications.tsx
+++ src/components/Publications/Publications.tsx
@@ -1,27 +1,45 @@
 import { Button, Card, CardActions, CardContent } from "@mui/material";
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import ContentSection from "components/Content/ContentSection";
+import { PUBLICATIONS } from "constants/publications";
 import './publications.css';
 
 type T_Props = {}
 
 const Publications: React.FC<T_Props> = (props) => {
+
+    const handleClick = (link: string) => {
+        window.open(link, "_blank")
+    }
+
   return (
     <ContentSection id='publications' title='Publications'>
-        <Card className='card'>
-            {PUBLICATIONS}
-            <CardContent>
-                <h3>The role of efficiency in the economic growth quality generation and manifestation processes</h3>
-                <p>
-                    «Регион и мир» · May 25, 2022
-                </p>
-                <Button startIcon={<OpenInNewIcon />} size='small'>
-                    See Publication
-                </Button >
-            </CardContent>
-            <CardActions>
-            </CardActions>
-        </Card>
+        <div className='publications-wrapper'>
+            {
+                PUBLICATIONS.map(pub => {
+                    return (
+                        <Card key={pub.id} className='publication-card'>
+                            <CardContent>
+                                <h3>
+                                    {pub.title}
+                                </h3>
+                                <p>
+                                    {`"${pub.journal}"`}
+                                </p>
+                                <p>
+                                    {pub.date}
+                                </p>
+                            </CardContent>
+                            <CardActions>
+                                <Button onClick={() => handleClick(pub.link)} startIcon={<OpenInNewIcon />} size='small'>
+                                    See Publication
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    )
+                })
+            }
+        </div>
     </ContentSection>
   );
 };
--- src/constants/publications.ts
+++ src/constants/publications.ts
@@ -3,7 +3,35 @@ export const PUBLICATIONS = [
         id: 1,
         title: 'The role of efficiency in the economic growth quality generation and manifestation processes',
         journal: 'Region and World',
-        date: 'May 25, 2022',
+        date: 'May, 2022',
         link: 'http://geopolitika.am/dir/wp-content/blogs.dir/1/files/2022_3_134_141.pdf'
-    }
+    },
+    {
+        id: 2,
+        title: 'The Malmquist productivity index and its analysis on the example of the RA',
+        journal: 'Sciences of Europe',
+        date: 'November, 2021',
+        link: 'https://www.doi.org/10.24412/3162-2364-2021-82-3-3-10'
+    },
+    {
+        id: 3,
+        title: 'Prosperity index of the RA and imitation model of prosperity evaluation',
+        journal: 'Economist',
+        date: 'March, 2021',
+        link: 'http://library.asue.am/open/gitajoxov_28_2.pdf'
+    },
+    {
+        id: 4,
+        title: 'Assessing the impact of macroeconomic indicators on the level of unemployment in RA',
+        journal: 'Alternative',
+        date: 'February, 2021',
+        link: 'http://alternative.am/wp-content/uploads/2021/04/%D5%84%D5%A1%D6%80%D5%BF-2021.pdf'
+    },
+    {
+        id: 5,
+        title: 'Dynamics and modern structure of the RA financial market',
+        journal: 'Region and World',
+        date: 'January, 2021',
+        link: 'http://geopolitika.am/dir/wp-content/blogs.dir/1/files/2021_1_116_121.pdf'
+    },
 ]
