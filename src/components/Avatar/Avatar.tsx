9adf2d85beb2bdd7c6ec9434456f3fa3e73d4772
NavasardianMichael <navasardianmichael@gmail.com>
2022-11-01 02:35:02 +0400

    avatar border is changed according to theme

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,11 +1,13 @@
 import avatar from 'assets/images/main-avatar.jpeg'
+import { useTheme } from "@mui/material";
 import './avatar.css'
 
 type T_Props = {}
 
 export default function Avatar (props: T_Props) {
+  const themeContext = useTheme()
   return (
-    <div className='avatar-wrapper'>
+    <div className={`avatar-wrapper ${themeContext.palette.mode === 'light' ? 'shadow' : 'border'}`}>
       <img src={avatar} />
       <h3 className='full-name'>Michael Navasardyan</h3>
     </div>
